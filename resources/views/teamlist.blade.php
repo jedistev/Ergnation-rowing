eca9f9c4b5e1eaf8e2a49bd76322c717d9bf809b
jedistev <jedistev@gmail.com>
2021-08-24 12:48:51 +0100

    Update teamlist.blade.php

--- resources/views/teamlist.blade.php
+++ resources/views/teamlist.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title', 'Data Tables')
+@section('title', 'User in team list')
 @section('content')
     <!-- push external head elements to head -->
     @push('head')
